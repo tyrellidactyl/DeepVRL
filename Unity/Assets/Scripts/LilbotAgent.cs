db4b97b5999a8d1b19fc6478cd75f19e6ada9dc1
Ty Lewis <43212441+tyrellidactyl@users.noreply.github.com>
2021-08-07 15:17:06 -0500

    Add files via upload

--- Unity/Assets/Scripts/LilbotAgent.cs
+++ Unity/Assets/Scripts/LilbotAgent.cs
@@ -8,29 +8,44 @@ public class LilbotAgent : Agent
 {
     Rigidbody rBody;
     public Transform Target;
-    public float acceleration;
-    public float maxSpeed = 5;
+    public float acceleration = 1;
+    public float maxSpeed = 1;
+    private bool collided = false;  
 
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
     }
-    
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            collided = true;
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Collision Detected, Stopping Test");
+            //Debug.Log(collided);
+        }
+    }
+
     public override void OnEpisodeBegin()
     {
-        if (this.transform.localPosition.y < 0)
-        {
-            // If the Agent fell, zero its momentum
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0, 0.125f, 0);
-            this.transform.rotation = new Quaternion(0, 0, 0, 0);
-        }
 
+        // Reset agent position
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3(3.0f, 0.125f, 0);
+        this.transform.rotation = new Quaternion(0, 0, 0, 0);
+        
         // Move the target to a new spot
         Target.localPosition = new Vector3(Random.value * 8 - 4,
-                                           0.5f,
+                                           0.0f,
                                            Random.value * 8 - 4);
+
+        // Reset collision property
+        collided = false;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -59,9 +74,22 @@ public class LilbotAgent : Agent
         float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
 
         // If agent reached target
-        if (distanceToTarget < 1.0f)
+        if (distanceToTarget < 0.5f)
         {
             SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // If agent exceeded domain boundaries
+        if (distanceToTarget > 20.0f)
+        {
+            Debug.Log("Domain Boundary Exceeded, Stopping Test");
+            EndEpisode();
+        }
+
+        // If collision occurred
+        if (collided == true)
+        {
             EndEpisode();
         }
 
@@ -70,6 +98,7 @@ public class LilbotAgent : Agent
         {
             EndEpisode();
         }
+
     }
 
     public override void Heuristic(float[] actionsOut)
